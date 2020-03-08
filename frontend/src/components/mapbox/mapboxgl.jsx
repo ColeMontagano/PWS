5d69bbe4362fe038273f2ca629387f4caffa3b46
Cole Montagano <cmontagano23@gmail.com>
2020-03-07 20:33:39 -0800

    get location with hooks

--- frontend/src/components/mapbox/mapboxgl.jsx
+++ frontend/src/components/mapbox/mapboxgl.jsx
@@ -5,21 +5,39 @@ import axios from 'axios'
 var MAPBOX_TOKEN = ''; // Set your mapbox token here
 
 export default function Mapbox() {
-  const [viewport, setViewport] = useState({
-    latitude: 37.8,
-    longitude: -122.4,
-    zoom: 14,
-    bearing: 0,
-    pitch: 0
-  });
+  const [viewport, setViewport] = useState();
   const [loading, setLoading] = useState(true)
 
   useEffect(() => {
             axios.get("http://localhost:8080/mapboxAPIKey").then(({data}) => {
             MAPBOX_TOKEN = data
-            setLoading(false)
+            getLocation()
         })
   }, [])
+
+  const getLocation = () => {
+    const options = {
+        enableHighAccuracy: false,
+        timeout: 5000,
+        maximumAge: 0
+    }
+    
+    const success = (pos) => {
+        var crd = pos.coords;
+        setViewport({
+            latitude : crd.latitude,
+            longitude : crd.longitude,
+            zoom: 10,
+            bearing: 0,
+            pitch: 0})
+        setLoading(false)
+    }
+    const error = (err) => {
+        console.warn(`ERROR(${err.code}): ${err.message}`);
+    }
+    navigator.geolocation.getCurrentPosition(success, error, options)
+
+}
 
   if (loading === true) {
       return <div>Loading...</div>
